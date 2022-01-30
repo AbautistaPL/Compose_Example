a0407be55ad1f7307d1d31968da77bb892427fce
AbautistaPL <Dish2013##>
2022-01-30 00:11:28 -0600

    fin del tutorial y se agrega CONVERSATION... una lista animada para CHATS.

--- app/src/main/java/com/raiserdev/compose_example/MainActivity.kt
+++ app/src/main/java/com/raiserdev/compose_example/MainActivity.kt
@@ -4,37 +4,52 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.dp
+import com.raiserdev.compose_example.data.SampleData
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme() {
-                MessageCard(Message("Alejandro", "Bautista"))
+                //MessageCard(SampleData.Message("Alejandro", "Bautista"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
 
-    data class  Message(val author: String, val lastName : String)
+    @Composable
+    fun Conversation(messages: List<SampleData.Message>){
+        LazyColumn{
+            items(messages) {
+                message ->
+                MessageCard(msg = message)
+            }
+        }
+    }
 
     @Composable
-    fun MessageCard( msg : Message){
+    fun MessageCard( msg : SampleData.Message){
         Row(
             modifier = Modifier.padding(all = 8.dp)
         )
@@ -56,10 +71,14 @@ class MainActivity : ComponentActivity() {
             )
 
             Spacer(modifier = Modifier.width(8.dp))
-            
-            Column{
+
+            var isExpanded by remember{ mutableStateOf(false)}
+            val surfaceColor : Color by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.secondaryVariant else MaterialTheme.colors.secondary
+            )
+            Column(modifier = Modifier.clickable { isExpanded =! isExpanded }){
                 Text(
-                    text = msg.author,
+                    text = msg.title,
                     color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle1
                 )
@@ -68,12 +87,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.height(4.dp)
                 )
 
-                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp, color = Color.Cyan) {
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
                     Text(
-                        text = msg.lastName,
+                        text = msg.body,
                         color = Color.Black,
                         style = MaterialTheme.typography.body2,
-                        modifier = Modifier.padding(all = 4.dp)
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
                     )
                 }
 
@@ -82,6 +108,8 @@ class MainActivity : ComponentActivity() {
 
 
     }
+
+
 
     @Preview(name = "Light Mode")
     @Preview(
@@ -92,7 +120,15 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewCard(){
         MaterialTheme() {
-            MessageCard(msg = Message("Otro Alex", "Otro LN"))
+            MessageCard(msg = SampleData.Message("Otro Alex", "Otro LN"))
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewConversation(){
+        MaterialTheme{
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 }
